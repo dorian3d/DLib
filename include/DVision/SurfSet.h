47d7ea5342bc518d12fe7ed23d612f4b366cf148
Dorian Galvez-Lopez <dorian@email.gwu.edu>
2015-03-02 22:11:30 +0100

    License changed to a modified BSD

--- include/DVision/SurfSet.h
+++ include/DVision/SurfSet.h
@@ -10,18 +10,7 @@
  *   used (in that case, both objects would share the Flann structure)
  *
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * License: see the LICENSE.txt file
  *
  */
 
